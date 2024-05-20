86529935777b105229edf2d8f050c3f09fd6373e
ashika-09 <157694214+ashika-09@users.noreply.github.com>
2024-05-20 16:51:18 +0000

    datatypess done

---
+++ Basics_01/datatypess.js
@@ -0,0 +1,19 @@
+"use strict"  //treat all js code as newer version
+
+console.log(3+3) 
+console.log("ashika")
+let state=true
+let temp=null
+
+
+/*
+  number , Boolean, string, undefine-->no assignment of values 
+  null--> standalone value
+  bigint--> for very large number
+  symbol--> for uniquness
+  object -->
+*/ 
+
+console.log(typeof null)  // object 
+console.log(typeof undefined)  // undefined
+
