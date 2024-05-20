e8b642148c0b9cb87812bc70ca06292cdf2a98b9
ashika-09 <157694214+ashika-09@users.noreply.github.com>
2024-05-20 16:36:20 +0000

    const/var done

---
+++ Basics_01/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144533
+let accountEmail="ash09@gmail.com"
+var accountPassword= "1234567" //we are not using var cux of issue in block and functional issue
+accountCity="Azamgarh"
+let accountState;  //if we declare variable but did not initialize value will be undefine
+accountEmail="asdf@gmail.com"
+accountPassword="64747"
+accountCity="bangalore"
+ console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
