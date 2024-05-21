2f68e84e852ec17e580cb39eb27d769cf2d8c4eb
ashika-09 <157694214+ashika-09@users.noreply.github.com>
2024-05-21 15:49:47 +0000

    conversion done

---
+++ Basics_01/conversionoperation.js
@@ -0,0 +1,32 @@
+// let score="33"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let value=Number(score)  //here we convert string to number
+// console.log(typeof value)
+
+// let age="33acd"
+// let val=Number(age)  //here we convert string to number
+// console.log(typeof val)
+// console.log(val)
+
+/*  "33"=>33
+   "33abc"=>nan
+   true=>1
+     "ash"=>nan
+     undefined=>nan
+     null=>0
+
+
+
+let num="3"
+let boolnum=Boolean(num)
+console.log(boolnum)*/
+
+// 1 or 5 or "asdd"=>true
+// 0 or " " =>false
+
+let somnum=5
+let numtostr=String(somnum)
+console.log(numtostr)
+
