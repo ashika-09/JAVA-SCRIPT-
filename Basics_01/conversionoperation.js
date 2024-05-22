6054443d40b06fb0c65cc103de978007a70b3d21
ashika-09 <157694214+ashika-09@users.noreply.github.com>
2024-05-22 14:31:09 +0000

    done

--- Basics_01/conversionoperation.js
+++ Basics_01/conversionoperation.js
@@ -26,7 +26,21 @@ console.log(boolnum)*/
 // 1 or 5 or "asdd"=>true
 // 0 or " " =>false
 
-let somnum=5
-let numtostr=String(somnum)
-console.log(numtostr)
+// let somnum=5
+// let numtostr=String(somnum)
+// console.log(numtostr)
+// **************************operation**********************
+
+/*let str1="good evening "
+let str2="ashika"
+let str3=str1+str2
+console.log(str3)
+console.log("1" + 2)
+console.log(2 + "1" + 2)
+console.log("1" + 2 +2)
+console.log(2 + 2 + "1" + 2)
+*/
+let num=100
+num++;
+console.log(num)
 
