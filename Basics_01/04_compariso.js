36be7cc6040e98281a11c99e93d6b7a13ace7169
ashika-09 <157694214+ashika-09@users.noreply.github.com>
2024-05-22 15:15:04 +0000

    comparision done

---
+++ Basics_01/04_compariso.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1);  diffrent datatypes is there
+// console.log("02">1);  gives unpredictable results
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log(undefined>0)
+console.log(undefined ==0)
+console.log(undefined>=0)
+// must avoid above comparisons cuz it 
+//quit confusion. here >= and == works diffrently
+// although === check datatypes and numbrs both
+
+console.log("2"===2)
