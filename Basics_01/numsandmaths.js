2e427f53f365dcad47c711345c6285a835f9bc2b
ashika-09 <157694214+ashika-09@users.noreply.github.com>
2024-05-25 13:18:15 +0000

    mathsdone

---
+++ Basics_01/numsandmaths.js
@@ -0,0 +1,8 @@
+const num=123.983
+//console.log(num.toPrecision(5));
+
+const hundreds=100000000
+//console.log(hundreds.toLocaleString('en-IN'));
+//console.log(Math.random());
+//console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10));
